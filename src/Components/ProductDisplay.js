bbd06343c5157f316c35d02cfbde6ae0bc4c7623
Santiago Báez Duic <santib1598@hotmail.com>
2022-09-10 21:24:06 -0300

    Cleaned a little bit of code

--- src/Components/ProductDisplay.js
+++ src/Components/ProductDisplay.js
@@ -1,36 +1,29 @@
-import { useEffect, useState, useContext } from "react";
+import { useState, useEffect } from "react";
 import ProductCard from "./ProductCard";
 import "../SCSS/productDisplay.scss";
-import { AppContext } from "../Context/AppContext";
 
 export default function ProductDisplay() {
-    const [ret, setRet] = useState(null);
-    const [productsURL, setProductsURL] = useState(null);
-    const {currentEmail} = useContext(AppContext);
-    console.log(currentEmail)
+    const [data, setData] = useState(null);
+
+    const getProducts = async () => {
+            const productsURL = `${process.env.REACT_APP_DATABASE_STRING}/products`;
+            const response = await fetch(productsURL)
+            const data = await response.json()
+            setData(data);
+    }
 
     useEffect(() => {
-        if(process.env.REACT_APP_DATABASE_STRING){
-            setProductsURL(`${process.env.REACT_APP_DATABASE_STRING}/products`)
-        }
+        getProducts();
+    }, [])
 
-        if(productsURL !== null){
-            fetch(productsURL)
-            .then((response) => response.json())
-            .then((data) => setRet(data))
-            .catch((error) => console.log(error))
-        }
-    }, [productsURL])
-    
     return(
         <div className="prddply-container">
             <h1 className="prddply-title">Products</h1>
             <div className="prddply-subcontainer">
-            {ret === null || ret === undefined ? <p>Something went wrong</p> : ret.map((obj) => {
+            {data === null || data === undefined ? <p>Something went wrong</p> : data.map((obj) => {
                 return (<ProductCard key={obj.code} obj={obj} />)
             })}
             </div>
-        {/* <button onClick={test}>test</button> */}
         </div>
     )
 }
--- src/Context/AppContext.js
+++ src/Context/AppContext.js
@@ -5,9 +5,8 @@ export const AppContext = createContext();
 export default function AppProvider({ children }){
     const [admin, setAdmin] = useState(false);
     const [logged, setLogged] = useState(false);
-    const [currentEmail, setCurrentEmail] = useState(undefined);
 
     return(
-        <AppContext.Provider value={{ admin, setAdmin, logged, setLogged, currentEmail, setCurrentEmail }}>{children}</AppContext.Provider>
+        <AppContext.Provider value={{ admin, setAdmin, logged, setLogged }}>{children}</AppContext.Provider>
     )
 }
