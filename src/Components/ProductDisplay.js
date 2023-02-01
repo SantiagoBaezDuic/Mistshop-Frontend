3a05821a1947185cd4da3f0a238915c4500ffb01
Santiago Báez Duic <santib1598@hotmail.com>
2023-02-01 14:18:32 -0300

    added a log

--- src/Components/ProductDisplay.js
+++ src/Components/ProductDisplay.js
@@ -16,7 +16,7 @@ export default function ProductDisplay() {
             productsURL = `${process.env.REACT_APP_DATABASE_STRING}/products`;
         }
         const response = await (await fetch(productsURL)).json();
-        console.log(response);
+        console.log(response.body);
         const data = await response.json();
         setData(data);
         if(catalogue === null){
