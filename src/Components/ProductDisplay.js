13ae7427b3e7af5a0eba403db4517717b81c26dd
Santiago Báez Duic <santib1598@hotmail.com>
2023-02-01 13:46:56 -0300

    added a mising await

--- src/Components/ProductDisplay.js
+++ src/Components/ProductDisplay.js
@@ -16,7 +16,7 @@ export default function ProductDisplay() {
             productsURL = `${process.env.REACT_APP_DATABASE_STRING}/products`;
         }
         const response = await fetch(productsURL);
-        const data = response.json();
+        const data = await response.json();
         setData(data);
         if(catalogue === null){
             setCatalogue(data);
